b88de930a544b3d8c2d89fbd406921838de0c7fb
Gianpierre <ggalvez92@hotmail.com>
2021-02-09 09:53:37 -0500

    Update docs build

---
+++ docs/precache-manifest.217cc137dfa5b7967158e8e8a4478428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e451ec1e8ebe2fd62316e195717d010",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14ae6f259009a895e53c",
+    "url": "/static/css/main.ed8271af.chunk.css"
+  },
+  {
+    "revision": "8c8f126e16e1c66a2c83",
+    "url": "/static/js/2.4a3582b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4a3582b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14ae6f259009a895e53c",
+    "url": "/static/js/main.e21550f5.chunk.js"
+  },
+  {
+    "revision": "0d4e3fbca24db45e5036",
+    "url": "/static/js/runtime-main.86719c39.js"
+  }
+]);
