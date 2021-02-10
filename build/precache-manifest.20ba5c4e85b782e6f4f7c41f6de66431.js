2fc13773da71ea4b3717e51ad1c2a50a5682be68
Gianpierre <ggalvez92@hotmail.com>
2021-02-09 19:17:49 -0500

    First commit

---
+++ build/precache-manifest.20ba5c4e85b782e6f4f7c41f6de66431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b293ba65b0f531f7c20dfdcc6c5cd2dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af89d9efea8b42a5878d",
+    "url": "/static/css/main.56ca42ed.chunk.css"
+  },
+  {
+    "revision": "0c3cd1469581550b5448",
+    "url": "/static/js/2.33837abf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.33837abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af89d9efea8b42a5878d",
+    "url": "/static/js/main.a3c9ec96.chunk.js"
+  },
+  {
+    "revision": "0d4e3fbca24db45e5036",
+    "url": "/static/js/runtime-main.86719c39.js"
+  }
+]);
